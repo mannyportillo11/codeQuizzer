b4a547e604e30390fda0ba234d07026a9ded88e1
Man E <mannyportillo77@gmail.com>
2021-08-29 23:05:21 -0600

    added styling, favicon, and js functions

--- assets/scripts.js
+++ assets/scripts.js
@@ -58,3 +58,149 @@ let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
 var questionCount = 0;
 
 let score = 0
+
+function setTime() {
+  displayQuestions();
+  let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "";
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      captureUserScore();
+    } 
+  }, 1000);
+}
+
+function displayQuestions() {
+  removeEls(startButton);
+
+  if (questionCount < questions.length) {
+    questionDiv.innerHTML = '<div class = "title-centered"> ' + questions[questionCount].title + '</div>';
+    choices.textContent = "";
+
+    for (let i = 0; i < questions[questionCount].multiChoice.length; i++) {
+      let el = document.createElement("button");
+      el.innerText = questions[questionCount].multiChoice[i];
+      el.setAttribute("data-id", i);
+      el.addEventListener("click", function (event) {
+        event.stopPropagation();
+
+        if (el.innerText === questions[questionCount].answer) {
+          score += secondsLeft;
+        } else {
+          score -= 10;
+          secondsLeft = secondsLeft - 10;
+        }
+        
+        questionDiv.innerHTML = "";
+
+        if (questionCount === questions.length) {
+          return;
+        } else {
+          questionCount++;
+          displayQuestions();
+        }
+      });
+      choices.append(el);
+    }
+  }
+}
+
+
+function captureUserScore() {
+  timer.remove();
+  choices.textContent = "";
+
+  let initialsInput = document.createElement("input");
+  let postScoreBtn = document.createElement("input");
+
+  results.innerHTML = '<div class = "youScored-center">' + `You scored ${score} points!` + "</br>" + `Enter initials:` + '</div>'
+  initialsInput.setAttribute("type", "text");
+  postScoreBtn.setAttribute("type", "button");
+  postScoreBtn.setAttribute("value", "Post My Score!");
+  postScoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+    let initials = initialsInput.value;
+    let userAndScore = {
+      initials: initials,
+      score: score,
+    };
+
+    scoresArray.push(userAndScore);
+    saveScores(scoresArray);
+    displayAllScores();
+    clearScoresBtn();
+    goBackBtn();
+    viewScoresBtn.remove();
+  });
+  results.append(initialsInput);
+  results.append(postScoreBtn);
+}
+
+const saveScores = (array) => {
+  window.localStorage.setItem("highScores", JSON.stringify(array));
+}
+
+const defineScoresArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+const removeEls = (...els) => {
+  for (let el of els) el.remove();
+}
+
+function displayAllScores() {
+  removeEls(timer, startButton, results);
+  let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+  scoresArray.forEach(obj => {
+    let initials = obj.initials;
+    let storedScore = obj.score;
+    let resultsP = document.createElement("p");
+    resultsP.innerText = `${initials}: ${storedScore}`;
+    scoresDiv.append(resultsP);
+  });
+}
+
+function viewScores() {
+  viewScoresBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    removeEls(timer, startButton);
+    displayAllScores();
+    removeEls(viewScoresBtn);
+    clearScoresBtn();
+    goBackBtn();
+  });
+}
+
+function clearScoresBtn() {    
+  let clearBtn = document.createElement("input");
+  clearBtn.setAttribute("type", "button");
+  clearBtn.setAttribute("value", "Clear Scores");
+  clearBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    removeEls(scoresDiv);
+    window.localStorage.removeItem("highScores");
+  })
+  scoresDiv.append(clearBtn)
+}
+
+function goBackBtn() {
+  let backBtn = document.createElement("input");
+  backBtn.setAttribute("type", "button");
+  backBtn.setAttribute("value", "Go Back");
+  backBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.reload();
+  })
+  buttonsDiv.append(backBtn)
+}
+
+viewScores();
